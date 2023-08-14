6fbfdbea6eeacce4617a730f9136f6504020bdd3
Priya-hub1 <priyaeswaran2807@gmail.com>
2023-08-14 16:49:26 +0530

    personal blog

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+ const App = () => {
+
+    return(
+        <div className="About-me"> 
+        <h3 className='heading'>About me </h3>
+        <p> 
+          I'm Priyatharshini.I ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb.
+          ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb, ndjfhjdfhdjhcjcvbb bbbbbbbbbbbb
+          ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb, ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb,v.bbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb,v
+        </p>
+     </div>
+    )
+   
+}
+
+export default App;
---
+++ src/career.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const Career = () => {
+
+  return(
+    <div className="About-me"> 
+    <h4 className='heading'>Career</h4>
+   <p>
+   I'm Priyatharshini.I ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb.
+          ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb, ndjfhjdfhdjhcjcvbb bbbbbbbbbbbb
+          ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb, ndjfhjdfhdjhcjcvbb bbbbbbbbbbbbbbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb,v.bbbbbdbcdhbcjdbcdjcdjdjbdbvdbdb,v
+
+   </p>
+    
+ </div>
+     
+  )
+}
+
+export default Career;
+
